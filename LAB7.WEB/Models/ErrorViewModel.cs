29eda2fed8d6e235bda3b6eb0d943da0c9964c98
HungHe198 <levanhung.1982004@gmail.com>
2024-10-12 04:26:56 +0700

    Revert "Add project files."
    This reverts commit 6cf8b582002c6dccc97b03bb7ad7ba296ab57c21.

--- LAB7.WEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LAB7.WEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
